0e01fe40524592f70e620b6bc348306a9744cecd
Leonardo Ezequiel Rodríguez <leonardoezequielr02mail.com>
2023-04-24 21:44:20 -0300

    primera versión funcional

--- code.js
+++ code.js
@@ -4,13 +4,18 @@ let secondTextarea = document.querySelector(".second-textarea");
 let workText;
 let encryptButton = document.querySelector(".encrypt");
 encryptButton.addEventListener("click", ()=>{
-    workText = firstTextarea.value;
-    let textWorked = process(workText);
-    secondTextarea.value = textWorked;
-    if(secondTextarea.value !== ''){
-        secondTextarea.classList.add('cursor');
+    if (/([A-Z]|[\u00E0-\u00FC]|[ÁÉÍÓÚ])/.test(firstTextarea.value) !== true) {
+        workText = firstTextarea.value;
+        let textWorked = process(workText);
+        secondTextarea.value = textWorked;
+        if(secondTextarea.value !== ''){
+            secondTextarea.classList.add('cursor');
+        }else {
+            secondTextarea.classList.remove('cursor')
+        }
     }else {
-        secondTextarea.classList.remove('cursor')
+        secondTextarea.value = '... se han detectado mayúsculas/acentos en el texto (carácteres no validos)';
+        secondTextarea.classList.remove('cursor');
     }
 });
 
@@ -43,7 +48,7 @@ function process(code) {
 secondTextarea.addEventListener('click', copy);
 
 function copy () {
-    if (secondTextarea.value !== '') {
+    if (secondTextarea.value !== '' && secondTextarea.value!== '... se han detectado mayúsculas/acentos en el texto (carácteres no validos)') {
         navigator.clipboard.writeText(secondTextarea.value).then(
             ()=> {
                 mergeMessage('copiado al portapapeles!')
@@ -60,7 +65,11 @@ let body = document.querySelector('body');
 function mergeMessage(message) {
     let fragment = document.createDocumentFragment();
     let mergeContainer = document.createElement('div');
-    mergeContainer.classList.add('merge-container');
+    if (body.clientWidth > '800') {
+        mergeContainer.classList.add('merge-container');
+    }else {
+        mergeContainer.classList.add('rsp-mrg-container');
+    }
     let msg = document.createElement('p');
     msg.textContent = message;
     msg.classList.add('merge-msg');
@@ -76,10 +85,13 @@ function mergeMessage(message) {
 let decryptButton = document.querySelector('.decrypt');
 let textToDecrypt;
 decryptButton.addEventListener('click', ()=>{
-    if(firstTextarea.value !== ''){
+    if(firstTextarea.value !== '' && /([A-Z]|[\u00E0-\u00FC]|[ÁÉÍÓÚ])/.test(firstTextarea.value) !== true){
         secondTextarea.classList.add('cursor')
         textToDecrypt = revertProcess(firstTextarea.value);
         secondTextarea.value = textToDecrypt;
+    }else if(/([A-Z]|[\u00E0-\u00FC]|[ÁÉÍÓÚ])/.test(firstTextarea.value) == true) {
+        secondTextarea.value = '... se han detectado mayúsculas/acentos en el texto (carácteres no validos)';
+        secondTextarea.classList.remove('cursor')
     }else {
         secondTextarea.value = '';
         secondTextarea.classList.remove('cursor')
